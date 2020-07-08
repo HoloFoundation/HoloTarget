cbfcb34d49adcb35c2abb7d8da7e78171f54c125
gonghonglou <gonghonglou@icloud.com>
2020-07-08 13:59:08 +0800

    match instance

--- HoloTarget/Classes/HoloNavigator.m
+++ HoloTarget/Classes/HoloNavigator.m
@@ -14,33 +14,31 @@ static char KHoloNavigatorParamsKey;
 @implementation HoloNavigator
 
 + (nullable UIViewController *)matchViewControllerWithProtocol:(Protocol *)protocol {
-    Class target = [[HoloTarget sharedInstance] matchTargetWithProtocol:protocol];
-    UIViewController *vc = [target new];
-    if ([vc isKindOfClass:UIViewController.class]) {
-        return vc;
-    } else if (vc) {
+    id target = [[HoloTarget sharedInstance] matchTargetInstanceWithProtocol:protocol];
+    if ([target isKindOfClass:UIViewController.class]) {
+        return target;
+    } else if (target) {
         if ([HoloTarget sharedInstance].exceptionProxy && [[HoloTarget sharedInstance] respondsToSelector:@selector(holo_matchFailedWithProtocol:)]) {
             [[HoloTarget sharedInstance].exceptionProxy holo_matchFailedWithProtocol:protocol];
         }
-        HoloLog(@"[HoloTarget] Match failed because the target (%@) is not kind of UIViewController, with the protocol (%@).", target, NSStringFromProtocol(protocol));
+        HoloLog(@"[HoloTarget] Match failed because the target (%@) is not kind of UIViewController, with the protocol (%@).", [target class], NSStringFromProtocol(protocol));
     }
     return nil;
 }
 
 + (nullable UIViewController *)matchViewControllerWithUrl:(NSString *)url {
-    Class target = [[HoloTarget sharedInstance] matchTargetWithUrl:url];
-    UIViewController *vc = [target new];
-    if ([vc isKindOfClass:UIViewController.class]) {
+    id target = [[HoloTarget sharedInstance] matchTargetInstanceWithUrl:url];
+    if ([target isKindOfClass:UIViewController.class]) {
         NSDictionary *targetParams = [url holo_targetUrlParams];
         if (targetParams) {
-            objc_setAssociatedObject(vc, &KHoloNavigatorParamsKey, targetParams, OBJC_ASSOCIATION_COPY_NONATOMIC);
+            objc_setAssociatedObject(target, &KHoloNavigatorParamsKey, targetParams, OBJC_ASSOCIATION_COPY_NONATOMIC);
         }
-        return vc;
-    } else if (vc) {
+        return target;
+    } else if (target) {
        if ([HoloTarget sharedInstance].exceptionProxy && [[HoloTarget sharedInstance] respondsToSelector:@selector(holo_matchFailedWithUrl:)]) {
            [[HoloTarget sharedInstance].exceptionProxy holo_matchFailedWithUrl:url];
        }
-        HoloLog(@"[HoloTarget] Match failed because the target (%@) is not kind of UIViewController, with the url (%@).", target, url);
+        HoloLog(@"[HoloTarget] Match failed because the target (%@) is not kind of UIViewController, with the url (%@).", [target class], url);
     }
     return nil;
 }
