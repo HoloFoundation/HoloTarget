a930a47a1294e6955c4199a0b57d2913439d2fbf
gonghonglou <gonghonglou@icloud.com>
2020-07-08 15:19:21 +0800

    comment

--- HoloTarget/Classes/HoloNavigator.h
+++ HoloTarget/Classes/HoloNavigator.h
@@ -11,10 +11,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HoloNavigator : NSObject
 
+/// 根据 protocol 匹配 viewController 实例
 + (nullable UIViewController *)matchViewControllerWithProtocol:(Protocol *)protocol;
 
+/// 根据 url 匹配 viewController 实例
 + (nullable UIViewController *)matchViewControllerWithUrl:(NSString *)url;
 
+/// 根据 viewController 匹配 url 入参
 + (nullable NSDictionary *)matchUrlParamsWithViewController:(UIViewController *)viewController;
 
 @end
--- HoloTarget/Classes/HoloTarget.h
+++ HoloTarget/Classes/HoloTarget.h
@@ -17,22 +17,31 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HoloTarget : NSObject
 
+/// 单例实现方法
 + (instancetype)sharedInstance;
 
+/// 异常代理
 @property (nonatomic, weak) id<HoloTargteExceptionProtocol> exceptionProxy;
 
+/// 获取各个 Pod 内的 holo_target.yaml 文件进行注册 protocol 和 url
 - (void)registAllTargetsFromYAML;
 
+/// 根据 protocol 注册 target 类
 - (BOOL)registTarget:(Class)target withProtocol:(Protocol *)protocol;
 
+/// 根据 url 注册 target 类
 - (BOOL)registTarget:(Class)target withUrl:(NSString *)url;
 
+/// 根据 protocol 匹配 target 类
 - (nullable Class)matchTargetWithProtocol:(Protocol *)protocol;
 
+/// 根据 url 匹配 target 类
 - (nullable Class)matchTargetWithUrl:(NSString *)url;
 
+/// 根据 protocol 匹配 target 实例
 - (nullable id)matchTargetInstanceWithProtocol:(Protocol *)protocol;
 
+/// 根据 url 匹配 target 实例
 - (nullable id)matchTargetInstanceWithUrl:(NSString *)url;
 
 @end
